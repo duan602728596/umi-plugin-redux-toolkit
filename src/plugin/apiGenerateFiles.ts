f6b0de2ae84e0def699b9ca2bd032be02f31c63f
段昊辰 <duanhaochen@126.com>
2020-11-02 10:27:08 +0800

    refactor: 读取模板文件

--- src/plugin/apiGenerateFiles.ts
+++ src/plugin/apiGenerateFiles.ts
@@ -7,8 +7,8 @@ import { getAllModels } from './getModels';
  * 读取文件
  * @param { string } file
  */
-async function readFile(file: string): Promise<string> {
-  return await fs.readFile(file, { encoding: 'utf8' });
+async function readTemplateFile(file: string): Promise<string> {
+  return await fs.readFile(path.join(__dirname, '../template', file), { encoding: 'utf8' });
 }
 
 /**
@@ -19,7 +19,7 @@ function apiGenerateFiles(api: IApi): void {
   async function onGenerateFiles(): Promise<void> {
     /* ============= options ============= */
     // options
-    const optionsContent: string = await readFile(path.join(__dirname, '../template/options.ts'));
+    const optionsContent: string = await readTemplateFile('options.ts');
     const ignoreOptions: string = api?.config?.ignoreOptions
       ? JSON.stringify(api.config.ignoreOptions, null, 2)
       : '{}';
@@ -41,25 +41,25 @@ export const sliceOptions: Array<Slice> = ${ modelsContent }`
     /* ============= 创建store ============= */
     api.writeTmpFile({
       path: 'redux-toolkit/store.ts',
-      content: await readFile(path.join(__dirname, '../template/store.ts'))
+      content: await readTemplateFile('store.ts')
     });
 
     /* ============= 创建runtime ============= */
     api.writeTmpFile({
       path: 'redux-toolkit/runtime.tsx',
-      content: await readFile(path.join(__dirname, '../template/runtime.tsx'))
+      content: await readTemplateFile('runtime.tsx')
     });
 
     /* ============= 创建utils ============= */
     api.writeTmpFile({
       path: 'redux-toolkit/utils.ts',
-      content: await readFile(path.join(__dirname, '../template/utils.ts'))
+      content: await readTemplateFile('utils.ts')
     });
 
     /* ============= 创建types.d.ts文件 ============= */
     api.writeTmpFile({
       path: 'redux-toolkit/types.d.ts',
-      content: await readFile(path.join(__dirname, '../template/types.d.ts'))
+      content: await readTemplateFile('types.d.ts')
     });
   }
 
