1fb19f6bccc5378bbce699d0bdfa43723fea642b
段昊辰 <duanhaochen@126.com>
2020-11-02 10:29:10 +0800

    refactor: 调整api.onGenerateFiles

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,8 +1,7 @@
-import { promises as fs } from 'fs';
 import * as path from 'path';
 import { utils, IApi } from 'umi';
 import describe from './describe';
-import { getAllModels } from './getModels';
+import apiGenerateFiles from './apiGenerateFiles';
 
 const { lodash: _ }: typeof utils = utils;
 
@@ -10,42 +9,7 @@ function umiPluginReduxToolkit(api: IApi): void {
   const { paths }: IApi = api;
 
   describe(api);
-
-  api.onGenerateFiles(async function(): Promise<void> {
-    // 创建store
-    api.writeTmpFile({
-      path: 'redux-toolkit/store.ts',
-      content: await fs.readFile(path.join(__dirname, '../template/store.ts'), { encoding: 'utf8' })
-    });
-
-    // runtime
-    api.writeTmpFile({
-      path: 'redux-toolkit/runtime.tsx',
-      content: await fs.readFile(path.join(__dirname, '../template/runtime.tsx'), { encoding: 'utf8' })
-    });
-
-    // options
-    const optionsContent: string = await fs.readFile(path.join(__dirname, '../template/options.ts'), {
-      encoding: 'utf8'
-    });
-    const ignoreOptions: string = api?.config?.ignoreOptions
-      ? JSON.stringify(api.config.ignoreOptions, null, 2)
-      : '{}';
-
-    // models
-    const models: Array<string> = await getAllModels(api);
-    const requireArray: Array<string> = models.map((item: string) => `  require('${ item }').default`);
-    const modelsContent: string = models.length > 0 ? `[
-${ requireArray.join(',\n') }
-]` : '[]';
-
-    api.writeTmpFile({
-      path: 'redux-toolkit/options.ts',
-      content: `${ optionsContent }
-export const ignoreOptions: IgnoreOptions = ${ ignoreOptions };
-export const sliceOptions: Array<Slice> = ${ modelsContent }`
-    });
-  });
+  apiGenerateFiles(api);
 
   api.addRuntimePlugin(function(): string {
     return path.join(paths.absTmpPath!, 'redux-toolkit/runtime.tsx');
