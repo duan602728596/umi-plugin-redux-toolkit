23060b0928c6f52ddcb993d7f74f3f617a8a6e96
段昊辰 <duanhaochen@126.com>
2020-11-04 13:35:36 +0800

    refactor: 修改插件注册名称

--- src/plugin/utils.ts
+++ src/plugin/utils.ts
@@ -2,7 +2,7 @@ import type { IApi } from 'umi';
 import { IgnoreOptions } from '../template/types';
 
 /* 插件注册名称 */
-export const pluginName: string = 'redux-toolkit';
+export const pluginName: string = 'reduxToolkit';
 
 /* 获取插件配置 */
 export interface PluginConfig {
